ffaa12f9a8e01341ecb7c0d7a6dfe10d707a09d3
Matthew Radford <matthewradford@Vitalwerkss-MacBook-Air.local>
2014-10-28 20:59:13 -0700

    Adding default comments if no param is provided

---
+++ witty-shit-array.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    "comments" => array(
+        "Y U NO BE BETTER DEVELOPER?",
+        "git gud",
+        "What were you thinking?",
+        "DO YOU WANT LEGACY-BASE?! BECAUSE THAT'S HOW YOU GET LEGACY BASE!",
+    )
+)
